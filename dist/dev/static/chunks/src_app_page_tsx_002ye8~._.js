f84efe6bfad8fa89bdb3fb0d525b09ff43e8d7ab
remy c <chenggedejing@outlook.com>
2026-04-14 16:40:56 +0800

    Clean up: remove build artifacts and unnecessary files, add GitHub Actions workflow for deployment

--- dist/dev/static/chunks/src_app_page_tsx_002ye8~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_02255~8._.js","static/chunks/node_modules_0ep5r.f._.js"],
-    source: "dynamic"
-});
